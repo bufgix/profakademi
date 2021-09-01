205d6a72c4f1dfa61ce7705f20927975bc50b99e
bufgix <ooruc471@yandex.com>
2021-09-01 18:37:04 +0300

    fix: remove backend code

--- components/Information/index.js
+++ components/Information/index.js
@@ -15,32 +15,11 @@ function Information() {
       <div className={cn(styles.information)}>
         <div className={cn(styles.announcements)}>
           <h2>DUYURULAR</h2>
-          <ul>
-            {/*  add loading */}
-            {!announcements.isLoading
-              ? announcements.data.map((announcement, index) => (
-                  <li key={index}>
-                    <ArrowIcon color="black"/>
-                    <Link href={`/duyurular/${announcement.id}`}>
-                      {announcement.baslik}
-                    </Link>
-                  </li>
-                ))
-              : null}
-          </ul>
+
         </div>
         <div className={cn(styles.results)}>
           <h2>SONUÇLAR</h2>
-          {/*  add loading */}
-          <ul>
-            {!results.isLoading
-              ? results.data.map((result, index) => (
-                  <li key={index}>
-                    <DocIcon /> <a href={result.pdflink}>{result.isim}</a>
-                  </li>
-                ))
-              : null}
-          </ul>
+
         </div>
       </div>
     </div>
